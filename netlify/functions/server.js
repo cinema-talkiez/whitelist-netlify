015367e59614a674616b4d13fa86b12f8ba225b2
Helloworld! <cinema.talkies.app2.io@gmail.com>
2025-07-27 18:41:36 +0530

    @

---
+++ netlify/functions/server.js
@@ -0,0 +1,20 @@
+
+const fs = require("fs");
+const path = require("path");
+
+exports.handler = async function () {
+  const filePath = path.join(__dirname, "../../public/server.json");
+  try {
+    const data = fs.readFileSync(filePath, "utf-8");
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "application/json" },
+      body: data,
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Could not read server.json" }),
+    };
+  }
+};
